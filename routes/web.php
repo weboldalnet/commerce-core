de324f6d5b099e240bb127c3139025e41b72adfa
Kerekes Ádám <adam.kerekes25@gmail.com>
2026-05-29 16:21:58 +0200

    init commit 2

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,15 @@ use App\Helpers\CustomPageHelper;
 use App\Http\Controllers\Admin\Article\ArticleController;
 
 Route::namespace('App\Http\Controllers\Site')->domain(getSiteDomain())->middleware('web', 'site_share')->group(function () {
-    /** ----- Blog funkciók ----- */
+    // Site route-ok helye
 
 });
 
 Route::namespace('App\Http\Controllers\Admin')->domain(getAdminDomain())->middleware('web', 'admin_share')->group(function () {
 
     Route::middleware('auth:admin')->group(function () {
+        // Admin route-ok helye
+        // Admin ha tovább vannak mappázva
         Route::namespace('Article')->group(function () {
 
         });
